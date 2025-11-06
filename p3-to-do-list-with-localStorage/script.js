fb38891784f1f85224efb4805bc00f1f98fade26
Cesar <cfitchf@wgu.edu>
2025-11-06 00:39:06 -0800

    created ids' for each list item and markTask() function done.

--- p3-to-do-list-with-localStorage/script.js
+++ p3-to-do-list-with-localStorage/script.js
@@ -12,21 +12,26 @@ function addTask(taskText) {
         return;
     }
 
-    const newList = document.createElement("li");
-    newList.textContent = taskText;
-    //newList.classList.add("");
-    taskList.appendChild(newList);
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+    taskList.appendChild(listItem);
+    let numId = taskList.children.length;
+    listItem.classList.add(`t${numId}`);
 }
 
 function clearAll() {
-
     if (taskList.children.length > 0) {
         taskList.innerHTML = '';
-    } 
+    }
 }
 
 function markTask(taskId) {
-
+    const listItem = document.querySelector(`.${taskId}`);
+    if (listItem.classList.contains("completed")) {
+        listItem.classList.remove("completed");
+    } else {
+        listItem.classList.add("completed");
+    }
 }
 
 function clearTask() {
@@ -39,4 +44,11 @@ addTaskBtn.addEventListener("click", () => {
     addTask(userInput);
 });
 
-clearAllBtn.addEventListener("click", clearAll)
+taskList.addEventListener("click", (event) => {;
+    const clickedElement = event.target;
+    const classList = clickedElement.className.split(" ");
+    const taskId = classList.find(c => c.startsWith("t"));
+    markTask(taskId);
+});
+
+clearAllBtn.addEventListener("click", clearAll);
